80ecd1258aac8160cf62719236ed523e02e287e5
Miguel Camilo <miguelcamilo48@gmail.com>
2023-01-26 13:03:13 -0500

    deployed express api with google cloud

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cloud Function Express API
+
+Deployed an Express API with Google Cloud Firebase
+
+## Lesson Learned
+
+1. API's
+2. Firebase
+3. Google Cloud Functions
+4. Express
+
+## See the the repo [click here.](https://github.com/MiguelCamilo/Deploy-Express-API)
---
+++ functions/index.js
@@ -0,0 +1,20 @@
+import functions from "firebase-functions"
+import express from "express"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+
+// setup routes
+app.get('/test', (req,res) => {
+    res.send('BIG DATA 🧠')
+})
+
+app.get('/test2', (req,res) => {
+    res.send('Confirm this route works✅')
+})
+
+// we have to export to open it up for the cloud to use
+// anytime we get any type of https req send it to express()
+export const api = functions.https.onRequest(app) 
+
